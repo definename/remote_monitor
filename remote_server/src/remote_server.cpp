22aad3234235f5613c18ae794dc50f985f592ab9
Oleh Kolomiitsev <kolomiytsev.oleg@gmail.com>
2021-05-12 22:26:51 +0300

    log macro fixes

--- remote_server/src/remote_server.cpp
+++ remote_server/src/remote_server.cpp
@@ -8,14 +8,14 @@ Poco::Logger::get(MONITOR_POCO_LOGGER_NAME).log(Poco::Message(				\
 		MONITOR_POCO_LOGGER_SOURCE, msg, Poco::Message::PRIO_INFORMATION))	\
 
 #define LOG_INF_FMT(fmt, ...)												\
-		NETLIB_FTL(Poco::format(fmt, ##__VA_ARGS__))						\
+		LOG_INF(Poco::format(fmt, ##__VA_ARGS__))							\
 
 #define LOG_ERR(msg)														\
 Poco::Logger::get(MONITOR_POCO_LOGGER_NAME).log(Poco::Message(				\
 		MONITOR_POCO_LOGGER_SOURCE, msg, Poco::Message::PRIO_ERROR))		\
 
 #define LOG_ERR_FMT(fmt, ...)												\
-		NETLIB_FTL(Poco::format(fmt, ##__VA_ARGS__))						\
+		LOG_ERR(Poco::format(fmt, ##__VA_ARGS__))							\
 
 #define LOG_WHAT2STR(e) std::string(e.what())
 
